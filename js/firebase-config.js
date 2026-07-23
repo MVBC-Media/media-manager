87cdec8dcc4f05029f8a50a76b81970b33e73af6
MVBC-Media <greatermtvernonbaptistchurch@gmail.com>
2026-07-22 19:28:34 -0500

    Reupload 2

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAgoEUcvJ6JqK1wdryTrvGSLF_1DkbnXYA",
+  apiKey: "AIzaSyAgoEUcvJ6JqKlwdryTrvGSLF_lDkbnXYA",
   authDomain: "gmvbc-media-manager.firebaseapp.com",
   projectId: "gmvbc-media-manager",
   storageBucket: "gmvbc-media-manager.firebasestorage.app",
   messagingSenderId: "722551210680",
   appId: "1:722551210680:web:04c48846c95994540f2a4d",
-  measurementId: "G-JMYVE77P09"
+  measurementId: "G-JMVYE77P09"
 };
